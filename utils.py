f2f0f91ca0ba8aa7719df59f2fb11c03df877f8d
Alexander Lobanov <wrider@yandex.ru>
2024-12-19 22:57:35 +0300

    Refactoring, added Readme and keyboard

---
+++ utils.py
@@ -0,0 +1,26 @@
+from emoji import emojize
+from random import randint, choice
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+
+import settings
+
+def get_smile(user_data):
+    if 'emoji' not in user_data:
+        smile = choice(settings.USER_EMOJI)
+        return emojize(smile, language='alias')
+    return user_data['emoji']
+
+
+def play_random_numbers(user_number):
+    bot_number = randint(user_number - 10, user_number + 10)
+    if user_number > bot_number:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}, ты выиграл!'
+    elif user_number == bot_number:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}, ничья!'
+    else:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}, я выиграл!'
+    return message
+
+
+def main_keyboard():
+    return ReplyKeyboardMarkup([['Прислать кота', KeyboardButton('Мои координаты', request_location=True)]])
